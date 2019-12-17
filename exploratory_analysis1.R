96dc772f444e9722b13d4a89a1f4264e50216af9
LSDRH <lauras14@ucm.es>
2019-12-17 18:17:26 +0100

    fixed project_report.Rmd to include preexisting images instead of r code chunks

--- exploratory_analysis1.R
+++ exploratory_analysis1.R
@@ -58,7 +58,6 @@ madrid_final$only_month <-lapply(madrid_final$date, month)
 madrid_final$only_year <-lapply(madrid_final$date, year)
 madrid_final$date<-as.POSIXct(madrid_final$date,format = "%Y-%m-%d %H:%M:%S", tz='CET')
 
-gg_miss_var(madrid_final, show_pct = T) #overall missing values
 selected_features <- madrid_final %>%  
   dplyr::select("date", "only_year", "only_month","station", "CO", "O_3", "PM10", "NO_2", "SO_2")
 
@@ -67,24 +66,24 @@ madrid_final_exp$only_year <- as.factor(madrid_final_exp$only_year)
 madrid_final_exp$only_month <- as.factor(madrid_final_exp$only_month)
 madrid_final_exp$station <- as.factor(madrid_final_exp$station)
 
-gg_miss_var(dplyr::select(madrid_final_exp, 4:8), show_pct = T) #missing values just for the 5 variables that we use
-
 #EXPLORATORY DATA ANALYSIS
 
 # 1. Center and distribution measures (boxplots and histograms)
 melt_madrid_final_1 <- melt(madrid_final_exp,id.vars='station', measure.vars=c('O_3','PM10','NO_2','SO_2', 'CO'))
 box_plot <- ggplot(melt_madrid_final_1,aes(x=variable, y=value, color=variable)) +
   geom_boxplot()+ coord_flip()+
-  scale_colour_manual(values=c("magenta3","goldenrod3","brown","lightslateblue", "green"))+
+  scale_colour_manual(values=c("magenta3","goldenrod3","brown","lightslateblue", "darkgreen"))+
   theme(legend.position="none")+scale_y_continuous(breaks = seq(0, 700, by = 50))+
   labs(title="Distribution of pollutants",x='Pollutants',y='ug/m3')
+box_plot
 histograms <-ggplot(melt_madrid_final_1,aes(x = value,fill=variable)) + 
   facet_wrap(~variable, nrow = 1) + 
   geom_histogram(binwidth=20)+
-  scale_fill_manual(values=c("magenta3","goldenrod3","brown","lightslateblue", "green"))+
+  scale_fill_manual(values=c("magenta3","goldenrod3","brown","lightslateblue", "darkgreen"))+
   theme(legend.position="none")+
   labs(x='ug/m3',y='Pollutants')
 
+histograms
 grid.arrange(box_plot,histograms,nrow=2)
 
 # 2. Linear relationships:
@@ -125,4 +124,4 @@ p2<-plot_scale_monthly(PM10,"PM10","goldenrod3")
 p3<-plot_scale_monthly(NO_2,"NO_2","brown")
 p4<-plot_scale_monthly(SO_2,"SO_2","lightslateblue")
 p5 <- plot_scale_monthly(CO,"CO","green")
-grid.arrange(p1,p2,p3,p4,p5,nrow=5,top = "Monthly Averaged on Daily Maximum Values")
+grid.arrange(p1,p2,p3,p4,p5,nrow=5,top = "Yearly Evolution of the Maximum Value of Pollutants each Month")
